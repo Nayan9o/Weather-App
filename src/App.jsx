dc4679fef2a4ab170e6e34ed1517e1eafcdd1a84
Nayan9o <nayanmeshram9o9@gmail.com>
2026-02-19 19:29:30 +0530

    setup the project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-blue-100">
+      <div className="bg-black/90 text-white rounded-lg shadow-lg p-8 max-w-md w-full">
+        <h1 className="text-3xl font-bold text-center ">Weather App </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
