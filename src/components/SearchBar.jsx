43c0206c7c9efa7cee113d063674623de240d959
Nayan9o <nayanmeshram9o9@gmail.com>
2026-02-19 19:43:24 +0530

    add Search input and button

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+const SearchBar = () => {
+  const [city, setCity] = useState("");
+  return (
+    <form className="flex">
+      <input
+        type="text"
+        placeholder="Enter city name"
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+        className="flex-1 p-2 border border-gray-300 rounded-l-lg outline-none border-r-0"
+      />
+      <button type="submit" className="bg-blue-500 border cursor-pointer p-2 hover:bg-blue-600 border-l-0 rounded-r-lg" >
+        Search
+
+      </button>
+    </form>
+  );
+};
+
+export default SearchBar;
